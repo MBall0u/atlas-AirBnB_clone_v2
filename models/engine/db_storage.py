5571fbe5d0ec1a6c1de8aacf8d241790c5579356
“MBall0u” <167779488+MBall0u@users.noreply.github.com>
2024-11-05 19:36:41 +0000

    M - trying to fix a circular import issue

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -56,4 +56,4 @@ class DBStorage:
         self.__session = Session()
 
     def close(self):
-        self.__session.remove()
+        self.__session.close()
--- models/state.py
+++ models/state.py
@@ -3,7 +3,6 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-from models.__init__ import HBNB_TYPE_STORAGE
 
 
 class State(BaseModel, Base):
