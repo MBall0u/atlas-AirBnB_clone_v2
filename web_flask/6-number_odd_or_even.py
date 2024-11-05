ff287830929f4e791fbe2b79d9f3444adc3f1867
“MBall0u” <167779488+MBall0u@users.noreply.github.com>
2024-11-05 16:09:30 +0000

    M - made the file number_odd_or_even.py and .html

---
+++ web_flask/6-number_odd_or_even.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python3
+"""Start of Web Framework"""
+from flask import Flask, abort, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_world():
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def display_hbnb():
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c(text):
+    return "C {}".format(' '.join(text.split('_')))
+
+
+@app.route("/python/", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python(text="is_cool"):
+    return "Python {}".format(' '.join(text.split('_')))
+
+
+@app.route("/number/<n>", strict_slashes=False)
+def is_number(n):
+    try:
+        is_int = int(n)
+        return "{} is a number".format(is_int)
+    except ValueError:
+        abort(404)
+
+
+@app.route("/number_template/<n>", strict_slashes=False)
+def number_template(n):
+    try:
+        is_int = int(n)
+        return render_template("5-number.html", num=n)
+    except ValueError:
+        abort(404)
+
+
+@app.route("/number_odd_or_even/<n>", strict_slashes=False)
+def number_odd_or_even(n):
+    try:
+        is_int = int(n)
+        if n % 2 == 0:
+            odd_or_even = "even"
+        else:
+            odd_or_even = "odd"
+        return render_template("6-number_odd_or_even.html", num=n, odd_or_even=odd_or_even)
+    except ValueError:
+        abort(404)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
